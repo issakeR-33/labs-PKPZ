28e0d1957a3bab5f49e2de01e026197e100338df
issakeR-33 <f60.c1tyx.tony@gmail.com>
2025-09-26 00:34:11 +0300

    done 3 task from lab

---
+++ laba2-task3/laba2-task3/laba2_task3.py
@@ -0,0 +1,36 @@
+﻿import random
+
+# Налаштування
+num_groups = 3
+num_students = 10
+num_exams = 4
+
+# Діапазон оцінок (приклад: від 1 до 100)
+min_grade = 1
+max_grade = 100
+
+# 1. Генеруємо оцінки для кожної групи
+groups = []
+for g in range(num_groups):
+    group = []
+    for s in range(num_students):
+        # Оцінки одного студента
+        grades = [random.randint(min_grade, max_grade) for _ in range(num_exams)]
+        group.append(grades)
+    groups.append(group)
+
+# average score of group
+group_averages = []
+for g_index, group in enumerate(groups):
+    total = 0
+    count = 0
+    for student in group:
+        total += sum(student)
+        count += len(student)
+    average = total / count
+    group_averages.append(average)
+    print(f"Group {g_index + 1} average: {average:.2f}")
+
+best_group_index = group_averages.index(max(group_averages))
+print(f"\nBest group is Group {best_group_index + 1} with average {group_averages[best_group_index]:.2f}")
+
