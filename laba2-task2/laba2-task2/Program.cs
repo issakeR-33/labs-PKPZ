838949b1578c8f7dcc7ffbac93544fd9e9dbbe2f
issakeR-33 <f60.c1tyx.tony@gmail.com>
2025-09-26 00:23:55 +0300

    done second trask from this lab

---
+++ laba2-task2/laba2-task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 7;
+        int[,] A = new int[n, n];
+        Random rnd = new Random();
+
+        // create matrix
+        Console.WriteLine("Initial matrix:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                A[i, j] = rnd.Next(-50, 50); // -50 ... +49
+                Console.Write($"{A[i, j],4}");
+            }
+            Console.WriteLine();
+        }
+
+        // min element
+        int minValue = A[0, 0];
+        int minCol = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (A[i, j] < minValue)
+                {
+                    minValue = A[i, j];
+                    minCol = j;
+                }
+            }
+        }
+
+        // print min element and its column
+        Console.WriteLine($"\nMinimum element: {minValue}, column: {minCol + 1}");
+
+   
+        int[,] B = new int[n, n]; // new matrix 
+        for (int j = 0; j < n; j++)
+        {
+            int newCol = (j - minCol + n) % n;
+            for (int i = 0; i < n; i++)
+            {
+                B[i, newCol] = A[i, j];
+            }
+        }
+
+        // Print result matrix
+        Console.WriteLine("\nMatrix after cyclic column shift:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{B[i, j],4}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
